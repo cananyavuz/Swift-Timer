eaeb14d4d381e7127a24071275506bb0ba5e326a
cananyavuz <yavuzcanan15@gmail.com>
2023-04-28 14:36:28 +0300

    Timer func used.

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    
+    
+    var timer = Timer()
+    var lastTime = 0
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lastTime = 15
+        Label.text =  "Zaman: \(lastTime)"
     }
 
-
+    @IBAction func startClick(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunc), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func timerFunc(){
+        
+        Label.text = "Zaman: \(lastTime)"
+        lastTime = lastTime - 1
+        
+        if (lastTime == -1){
+            Label.text = "Süre Bitti."
+            timer.invalidate()
+            lastTime = 15
+        }
+        
+    }
 }
 
